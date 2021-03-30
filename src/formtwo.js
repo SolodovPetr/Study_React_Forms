a01463c9d65eeb030f713b6264ce30e9a3c10128
Petr <solodov.petr@gmail.com>
2021-03-30 15:49:14 +0300

    Formik - Hook useFormik

--- src/formtwo.js
+++ src/formtwo.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const FormTwo = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            firstname: ''
+        },
+        validationSchema: Yup.object({
+            firstname: Yup.string()
+                .min(2, 'Too Short!')
+                .required('Field is Required')
+        }),
+        onSubmit: (values) => console.log('Submit', values)
+    });
+
     return(
         <div className="container">
             <div className="col-md-12 mt-5">
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <label htmlFor="firstname">First name</label>
-                    <input 
-                        className="form-control" 
-                        type="text" 
-                        name="firstname"
+                    <input className="form-control" type="text"
+                        /* name, value, onChange, onBlur */
+                        { ...formik.getFieldProps('firstname') }
                     />
+                    { formik.errors.firstname && formik.touched.firstname &&
+                    ( <span>{formik.errors.firstname}</span> ) }
                     
                     <hr className="mb-4" />
                     <button className="btn btn-primary btn-lg btn-block" type="submit">
