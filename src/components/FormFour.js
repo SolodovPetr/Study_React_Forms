42f274bfb1fd4128567b88e26fa3a07552ccb6b3
Petr <solodov.petr@gmail.com>
2021-03-31 16:58:28 +0300

    Form submit handler

--- src/components/FormFour.js
+++ src/components/FormFour.js
@@ -6,8 +6,8 @@ import { validate } from '../utils/validate';
 export default class FormFour extends Component {
 
     state = {
+        loading: false,
         formData: {
-            loading: false,
             name: {
                 element: 'input',
                 value: '',
@@ -19,6 +19,22 @@ export default class FormFour extends Component {
                 validation: {
                     required: true,
                     min: 3
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            },
+            lastname: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'lastname_input',
+                    type: 'text',
+                    placeholder: 'Enter your last name'
+                },
+                validation: {
+                    required: true,
+                    min: 4
                 },
                 valid: false,
                 touched: false,
@@ -44,10 +60,62 @@ export default class FormFour extends Component {
         }
 
         newFormData[field.id] = newFieldData;
+        this.setState({ formData: newFormData });
 
-        this.setState({
-            formData: newFormData
-        })
+        return newFieldData;
+    }
+
+    submitForm = e => {
+        e.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+        const { formData } = this.state;
+
+        for ( let key in formData ) {
+            formIsValid = formData[key].valid && formIsValid;
+        }
+
+        if ( formIsValid ) {
+            this.setState({ loading: true });
+            // collect fields data
+            for( let key in formData ) {
+                dataToSubmit[key] =  formData[key].value
+            }
+
+            // save data to db etc.
+            console.log('Submitted data:', dataToSubmit);
+
+            // emulate server request
+            setTimeout(() => {
+                this.setState({ loading: false });
+                this.onSuccess();
+            }, 1000);
+
+        } else {
+            // Show fields errors
+            let checkedFormData = {};
+            for( let key in formData ) {
+                const element = { e:{target:{value: formData[key].value}}, id: key, blur: true };
+                checkedFormData[key] = this.updateForm(element);
+            }
+            this.setState({formData: checkedFormData})
+            alert('Sorry, the form is not valid')
+        }
+    }
+
+    onSuccess = () => {
+        let formDataCopy = { ...this.state.formData }
+
+        // set data to default
+        for ( let key in this.state.formData ) {
+            formDataCopy[key].value = '';
+            formDataCopy[key].valid = false;
+            formDataCopy[key].touched = false;
+            formDataCopy[key].validationMessage = '';
+        }
+        this.setState({ formData: formDataCopy });
+        alert('Thank you');
     }
 
     render() {
@@ -61,6 +129,24 @@ export default class FormFour extends Component {
                                    change={ this.updateForm }
                         />
                     </div>
+
+                    <div className="form-group">
+                        <label htmlFor="name">Lastname</label>
+                        <FormField id="lastname"
+                                   fieldData={this.state.formData.lastname}
+                                   change={ this.updateForm }
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={ e => this.submitForm(e) }
+                        disabled={this.state.loading}
+                    >
+                        Submit
+                    </button>
+
                 </form>
             </div>
         )
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,21 +4,21 @@ export const validate = field => {
      * @description - [ isValid, error_message ]
      * @type {array}
      */
-    let error = [ false, '' ];
+    let valid = [ true, '' ];
 
     // Minimum length
     if ( field.validation.min && field.validation.min > 0 ) {
         const isValid = field.value.trim().length >= field.validation.min;
         const message =  isValid ? '' : `Minimum ${field.validation.min} char(s) required.`;
-        error = isValid ? error : [ isValid, message ];
+        valid = isValid ? valid : [ isValid, message ];
     }
 
     // Required
     if ( field.validation.required ) {
         const isValid = field.value.trim() !== '';
         const message =  isValid ? '' : 'This field is required';
-        error = isValid ? error : [ isValid, message ];
+        valid = isValid ? valid : [ isValid, message ];
     }
 
-    return error;
+    return valid;
 }
